11f09c0ea496bf190aa8ea58d054a57f21fd87b4
Cbet0mup <cbet0mup.pro@gmail.com>
2021-02-07 23:41:03 +0700

    начало проекта

---
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.sweater.config;
+
+import net.bytebuddy.asm.Advice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;  //нужен что бы менеджер мог искать в бд роли юзеров
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                //система  ищет юзера по имени
+                .usersByUsernameQuery("select username, password, active from usr where username=?")
+                //этот запрос позволяет спрингу получить список юзеров с их ролями
+                .usersByUsernameQuery("select u.username ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username = ?");
+    }
+}
